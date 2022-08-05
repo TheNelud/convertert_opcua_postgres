c11798ac22efacd68ffd8ffc32ad23c72b0906ad
TheNelud <riwok@bk.ru>
2022-08-05 16:04:29 +0300

    добавил сервер для тестирования

---
+++ ServerOPCUA/server.py
@@ -0,0 +1,51 @@
+# self.array_folder = ['GD06.UF01UD01.KS01.GPA11.MPA.CTM_POP_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.MPA.CTMA_D.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CPVD_SGG_ATM.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CS_GG.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPN.СTGB_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CPVD_SGG_ATM.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPN.СPGA_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CPVD_PGG_ATM.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CTGB_ST_MAX.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CPVB_K.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CS_GG_PRIV.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPN.CT_UP_RK_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.MPA.CTMB_OGGT.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CPVB_K.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPN.CT_UP_UK_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GQK.CFG_PO.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GQK.CFG_CGU_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.MPA.СPMA_D.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPN.СPGB_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CPVA_D.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CPVA_D.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPN.СPGD_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPN.СPGA_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.MPA.CTMB_OGGK.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CPVB.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.MPA.СPM_UP_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPN.СTGA_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPN.СPGA_SGU.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CPVB.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GQK.CFGT_M.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CTGB_GG_AVR.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.MPA.СPM_OP_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.MPA.CTM_POP_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CPGB_TGG_PLN.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CPGB_TGG_PLN.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CTVA_D.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPN.СPGD_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CPSUF.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.MPA.CTM_ZOP_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CTGB_GG_AVR.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.MPA.CTMB_PST.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GQK.CFGT.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPN.СTGB_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CPVD_ST_ATM.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.MPA.CTM_MBD.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CPVD_PGG_ATM.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPN.СTGA_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPN.CT_UP_RK_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.MPA.СPMA_D.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CTGB_ST.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CTVA_D.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CS_ST.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.MPA.СPM_OP_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CS_GG.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CTGB_ST_MAX.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CTGB_ST.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CPSUF.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPN.СPGA_SGU.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPN.CT_UP_UK_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.MPA.СPM_UP_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPN.СPGB_N.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.MPA.CTM_MBD.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GQK.CFG_CGU_V.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CS_GG_PRIV.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA12.GPT.CPVD_ST_ATM.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.MPA.CTMA_D.Socket_IOS.Value', 'GD06.UF01UD01.KS01.GPA11.GPT.CS_ST.Socket_IOS.Value']
+
+from http import server
+from random import choice, randint, random, uniform, getrandbits
+import time
+from tokenize import Double
+from opcua import ua, Server, Client
+import arrayList
+
+class OpcUaServer():
+    def __init__(self) -> None:
+        self.server = Server()
+        self.server.set_endpoint('opc.tcp://127.0.0.1:5555')
+        self.server.set_server_name('Server Emulator Gazprom Auto')
+        # self.idx = self.server.register_namespace('Value Emulation param')
+        self.node = self.server.get_objects_node()
+        self.array_folder =arrayList.array_nadim
+        self.i = -1
+
+
+    def addFolder(self):
+        for self._element in self.array_folder:
+            self.myFolder = self.node.add_variable('ns=1;s='+str(self._element),str(self._element), 0,varianttype=ua.VariantType.Float)
+            self.myFolder.set_writable()
+            self.randomValue()
+        
+    
+    def start(self):
+        self.server.start()
+        print("Server start!")
+        self.addFolder()
+        
+
+    def randomValue(self):
+        
+        self.myFolderValue = uniform(13.3, 25.4)
+        # self.myFolderValue = choice(['True', 'False'])
+        self.myFolder.set_value(self.myFolderValue, ua.VariantType.Float)
+
+
+    def stop(self):
+        self.server.stop()
+    
+
+
+if __name__ == "__main__":
+    # try:
+        my_server = OpcUaServer()
+        my_server.start()
+    # finally:
+    #     my_server.stop()
